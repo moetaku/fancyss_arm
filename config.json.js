0a2997607d8eb0bd74b357162b0254096edd36cb
moetaku <moetaku.163.com>
2021-06-14 18:28:55 +0800

    fix wget rule bug

--- config.json.js
+++ config.json.js
@@ -1,8 +1,8 @@
 {
-"build_date":"2021-06-14_18:19:54",
+"build_date":"2021-06-14_18:26:52",
 "description":"科学上网",
 "home_url":"Main_Ss_Content.asp",
-"md5":"72b7c3a4a6d7bab658d4ee469c21add3",
+"md5":"b4a3f7520a0d31b07e97d0a98c936fe9",
 "name":"shadowsocks",
 "tar_url": "https://raw.githubusercontent.com/moetaku/fancyss_arm/master/shadowsocks.tar.gz", 
 "title":"科学上网",
