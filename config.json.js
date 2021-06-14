7eda635be6bc18dfd8f402c7ae1f5e24ea9df970
moetaku <moetaku.163.com>
2021-06-14 18:52:31 +0800

    update package

--- config.json.js
+++ config.json.js
@@ -1,8 +1,8 @@
 {
-"build_date":"2021-06-14_18:26:52",
+"build_date":"2021-06-14_18:52:06",
 "description":"科学上网",
 "home_url":"Main_Ss_Content.asp",
-"md5":"b4a3f7520a0d31b07e97d0a98c936fe9",
+"md5":"74ec3b2f5987ea0d8fd64f2254408461",
 "name":"shadowsocks",
 "tar_url": "https://raw.githubusercontent.com/moetaku/fancyss_arm/master/shadowsocks.tar.gz", 
 "title":"科学上网",
