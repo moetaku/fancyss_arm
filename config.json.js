a4e4d89b971a7f4f96a591278c869c3b8072933f
moetaku <moetaku.163.com>
2021-06-14 16:24:10 +0800

    update tar.gz

--- config.json.js
+++ config.json.js
@@ -1,10 +1,10 @@
 {
-"build_date":"2020-01-01_23:00:07",
+"build_date":"2021-06-14_16:22:35",
 "description":"科学上网",
 "home_url":"Main_Ss_Content.asp",
-"md5":"c010d6a751cc92d18e88772375611477",
+"md5":"bd6d6e75b4a23e7609309a0db1ebfb34",
 "name":"shadowsocks",
-"tar_url": "https://raw.githubusercontent.com/hq450/fancyss/master/fancyss_arm/shadowsocks.tar.gz", 
+"tar_url": "https://raw.githubusercontent.com/moetaku/fancyss_arm/master/shadowsocks.tar.gz", 
 "title":"科学上网",
-"version":"4.2.2"
+"version":"4.2.3"
 }
